3316e6011ba8a09ebac03cc8f2aa6fb99baf4f93
Vabeeshan <vabeeshanvasan2002@gmail.com>
2026-03-09 21:23:41 +0530

    Initialising the project Structure for application

---
+++ src/main/java/com/weather/Weather_Analytics/WeatherAnalyticsApplication.java
@@ -0,0 +1,13 @@
+package com.weather.Weather_Analytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherAnalyticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherAnalyticsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/weather/Weather_Analytics/WeatherAnalyticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weather.Weather_Analytics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherAnalyticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
